c3530237c8c5abcb3a1cd13d9a8d157e8a20eaea
Sail <865605793@qq.com>
2017-10-23 19:04:44 +0800

    变量名bug待修复

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // import {tokenizer} from './main'
 let {tokenizer } = require ('./main');
 
-let input = "abcd @b";
+// let input = "abcd @b";
+let input = "abcd @b 123 \n";
+console.log(input);
 tokenizer(input);
