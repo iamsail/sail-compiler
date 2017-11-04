e413f15a2ba83dcdc2c6a3d51b3fa1b68e760c4f
Sail <865605793@qq.com>
2017-11-04 10:24:04 +0800

    递归下降语法分析搭建

---
+++ src/compiler/RecursiveDecline/program.js
@@ -0,0 +1,26 @@
+let analysisString = {
+    error : false,
+    info: ""
+};
+
+let program = () => {
+    analysisString.info = `progarm-->block\n`;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {program};
